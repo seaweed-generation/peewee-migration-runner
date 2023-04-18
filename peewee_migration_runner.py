080aa74eee0632ac0e2ed101f82982ae61b48a28
Dan Schaub <dan@danschaub.com>
2023-04-17 21:07:03 -0400

    add a test

--- peewee_migration_runner.py
+++ peewee_migration_runner.py
@@ -70,7 +70,8 @@ class MigrationRunner:
 
         if not rollback:
             raise NameError(
-                f'Cannot rollback with {migration_file.name}, down function not defined')
+                f'Cannot rollback with {migration_file.name}, down function not defined'
+            )
 
         with self.db.atomic():
             rollback(self.pw_migrator)
@@ -82,8 +83,7 @@ class MigrationRunner:
         file_globals = {}
         exec(ast, file_globals, None)
 
-        if not file_globals['up']:
-            raise NameError(
-                'Migration file does not contain an up function.')
+        if 'up' not in file_globals:
+            raise NameError('Migration file does not contain an up function.')
 
-        return file_globals['up'], file_globals['down']
+        return file_globals['up'], file_globals.get('down')
---
+++ tests/test_runner.py
@@ -0,0 +1,71 @@
+import pytest
+from peewee import SqliteDatabase, Table, Model, AutoField
+from peewee_migration_runner import MigrationRunner
+from pathlib import Path
+
+
+class MyTable(Model):
+    id = AutoField()
+
+
+@pytest.fixture
+def db(tmp_path):
+    _db = SqliteDatabase(tmp_path / 'test.db')
+
+    with _db.connection_context():
+        with MyTable.bind_ctx(_db):
+            MyTable.create_table(safe=False)
+
+            yield _db
+
+            MyTable.drop_table(safe=False)
+
+
+@pytest.fixture
+def migrations_path(tmp_path):
+    path: Path = tmp_path / 'migrations'
+    path.mkdir(exist_ok=True)
+    return path
+
+
+@pytest.fixture
+def runner(db, migrations_path):
+    return MigrationRunner(db, migrations_path)
+
+
+@pytest.fixture(autouse=True)
+def migration_file(migrations_path):
+    migration_text = """
+from playhouse.migrate import *
+import peewee as pw
+
+def up(migrator):
+    migrate(migrator.add_column('mytable', 'testcolumn', pw.IntegerField(default=0)))
+
+def down(migrator):
+    migrate(migrator.drop_column('mytable', 'testcolumn'))
+"""
+
+    path = migrations_path / '001_AddColumn.py'
+    path.write_text(migration_text)
+
+
+def test_up_and_down(db, runner):
+    runner.run()
+
+    columns = db.get_columns('mytable')
+    assert len(columns) == 2
+    assert any(c.name == 'testcolumn' for c in columns)
+
+    migration_table = Table('schemamigration')
+    migration_table.bind(db)
+    filename, = migration_table.select(
+        migration_table.c.filename
+    ).tuples().first()
+    assert filename == '001_AddColumn.py'
+
+    runner.rollback()
+    columns = db.get_columns('mytable')
+    assert len(columns) == 1
+    assert columns[0].name == 'id'
+    assert migration_table.select('1').count() == 0
